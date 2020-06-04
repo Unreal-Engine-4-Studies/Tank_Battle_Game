31f2dc7e2a3f3ed3621fd2bffcc0406ef38a8031
LeoFoxCruz <leonardorbc@gmail.com>
2020-06-03 22:52:40 -0400

    Refactoring.

--- Battle_Tank_Game/Source/Battle_Tank_Game/TankBarrel.cpp
+++ Battle_Tank_Game/Source/Battle_Tank_Game/TankBarrel.cpp
@@ -3,9 +3,7 @@
 #include "TankBarrel.h"
 
 void UTankBarrel::Elevate(float RelativeSpeed)
-{
-	UE_LOG(LogTemp, Warning, TEXT("Barrel elevation: %f"), RelativeSpeed);
-	
+{	
 	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, 1);
 	auto ElevationChange = RelativeSpeed * MaxDegreesPerSeconds * GetWorld()->DeltaTimeSeconds;
 	auto RawNewElevation = GetRelativeRotation().Pitch + ElevationChange;
